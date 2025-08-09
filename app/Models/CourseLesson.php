1ef498d1096bc1541eeccae88801d03f0975aa7e
Bertrand Wijaya <117497109+BBertt@users.noreply.github.com>
2025-08-09 18:20:04 +0700

    :card_file_box: initialize migrations & models (#7)
    * :card_file_box: Added courses tables
    
    * :card_file_box: Add User Interaction tables
    
    * :card_file_box: Add Transaction and Chat Tables
    
    * :card_file_box: Add Forum and Certification tables

---
+++ app/Models/CourseLesson.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseLesson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'course_section_id',
+        'title',
+        'description',
+        'video_url',
+    ];
+
+    public function section()
+    {
+        return $this->belongsTo(CourseSection::class);
+    }
+}
