e6e3dfeb7387e298b5b2e714461b2801820b0e0d
Joddy0 <143619313+Joddy0@users.noreply.github.com>
2025-09-20 17:46:39 +0700

    update: change logo and icons

--- resources/js/Pages/Landing.jsx
+++ resources/js/Pages/Landing.jsx
@@ -1,7 +1,7 @@
 import { Link } from "@inertiajs/react";
 
 const GuestNavbar = () => {
-    const logoSrc = "/assets/logo.svg";
+    const logoSrc = "/assets/logo.png";
     return (
         <nav className="bg-accent shadow-sm sticky top-0 z-50">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -18,7 +18,7 @@ const GuestNavbar = () => {
                     </div>
 
                     {/* Menu */}
-                    <div className="hidden md:flex items-center space-x-6 font-extrabold">
+                    <div className="hidden md:flex items-center space-x-10 font-extrabold">
                         <Link
                             href="#"
                             className="text-[#3D3D3D] hover:text-[#4F6D40]"
@@ -41,7 +41,7 @@ const GuestNavbar = () => {
                         {/* Login Button */}
                         <Link
                             href="/login"
-                            className="px-4 py-2 rounded-full text-white bg-[#4F6D40] hover:bg-[#3D3D3D] hover:text-white"
+                            className="px-4 py-2 rounded-xl text-white bg-[#4F6D40] hover:bg-[#3D3D3D] hover:text-white"
                         >
                             Log In
                         </Link>
