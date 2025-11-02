6da6e4da0868919c7d79883c0d36838b8429806d
Bertrand Valentino Wijaya <bertrandwijaya05@gmail.com>
2025-11-02 20:04:22 +0700

    ✨ feat(authentication): update verify email page

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,50 +1,66 @@
-import PrimaryButton from '@/Components/PrimaryButton';
-import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import AuthPagesLayout from "@/Layouts/AuthPagesLayout";
 
-export default function VerifyEmail({ status }) {
+import { Head, Link, useForm } from "@inertiajs/react";
+
+const VerifyEmail = ({ status }) => {
     const { post, processing } = useForm({});
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('verification.send'));
+        post(route("verification.send"));
     };
 
     return (
-        <GuestLayout>
-            <Head title="Email Verification" />
+        <div className="w-full max-w-xs flex flex-col gap-6">
+            <div className="text-center">
+                <h1 className="text-5xl font-bold">Verify Your Email</h1>
+            </div>
 
-            <div className="mb-4 text-sm text-gray-600">
+            <div className="text-center text-sm text-white">
                 Thanks for signing up! Before getting started, could you verify
                 your email address by clicking on the link we just emailed to
                 you? If you didn't receive the email, we will gladly send you
                 another.
             </div>
 
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 text-sm font-medium text-green-600">
+            {status === "verification-link-sent" && (
+                <div className="text-center text-sm font-medium text-green-600">
                     A new verification link has been sent to the email address
                     you provided during registration.
                 </div>
             )}
 
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>
-                        Resend Verification Email
-                    </PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        Log Out
-                    </Link>
-                </div>
+            <form onSubmit={submit} className="flex flex-col gap-6">
+                <button
+                    type="submit"
+                    disabled={processing}
+                    className="p-2 font-extrabold rounded-full bg-secondary hover:bg-[#000000] w-full"
+                >
+                    Resend Verification Email
+                </button>
             </form>
-        </GuestLayout>
+
+            <div className="flex flex-col items-center gap-4">
+                <div className="flex items-center w-full">
+                    <div className="w-1/2 border-t border-white"></div>
+                    <span className="px-3 text-sm">OR</span>
+                    <div className="w-1/2 border-t border-white"></div>
+                </div>
+
+                <Link
+                    href={route("logout")}
+                    method="post"
+                    as="button"
+                    className="p-2 text-center text-[#4F6D40] font-extrabold rounded-full bg-[#FFFFFF] hover:bg-[#3D3D3D] hover:text-white w-full"
+                >
+                    Log Out
+                </Link>
+            </div>
+        </div>
     );
-}
+};
+
+VerifyEmail.layout = (page) => <AuthPagesLayout children={page} />;
+
+export default VerifyEmail;
